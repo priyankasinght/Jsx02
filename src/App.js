ad62d799dd6f26c70997295253a0c03161bea017
priyankasinght <35567382+priyankasinght@users.noreply.github.com>
2022-11-01 15:36:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Text">
+      <h1>The Evoluation of Airbnb's Frontend</h1>
+      <h5>@spikebrehm</h5>
+      <img src="https://cdn.iconscout.com/icon/free/png-256/airbnb-5-461772.png" alt="" width="40" height="50"></img>
+      <p>airbnb</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
